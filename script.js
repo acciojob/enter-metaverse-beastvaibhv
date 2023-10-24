a2f552a73eb55fa330c5b085df562ca6410a4320
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-24 13:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const mybtn = document.getElementById("enterBtn");
+mybtn.addEventListener("click", change);
+const para = document.getElementById("status");
+function change(){
+	para.textContent = "";
+	const changedElement = document.createElement("h1");
+	changedElement.textContent = "Entered Metaverse";
+	para.appendChild(changedElement);
+}
